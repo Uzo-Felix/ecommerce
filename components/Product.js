6d1c5064f862872ed99fbdb50dd02a86a8f562cb
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-04-25 17:34:57 +0100

    fix: image tag

--- components/Product.js
+++ components/Product.js
@@ -9,7 +9,7 @@ export default function Product({_id,name,price,description,picture}) {
   return (
     <div className="w-52">
       <div className="bg-blue-100 p-5 rounded-xl">
-        <img src={picture} alt=""/>
+        <Image src={picture} alt=""/>
       </div>
       <div className="mt-2">
         <h3 className="font-bold text-lg">{name}</h3>
